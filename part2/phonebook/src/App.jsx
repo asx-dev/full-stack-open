0d6f1de9d21de571f758559fee2765f4e7109f07
asx-dev <asxdev18@gmail.com>
2024-08-30 08:54:32 +0100

    Part 2 Phone book step 4 completed Filter Functionality

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,10 +2,14 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-1234567" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [searchValue, setSearchValue] = useState("");
 
   const nameHandler = (e) => {
     setNewName(e.target.value);
@@ -13,6 +17,10 @@ const App = () => {
 
   const numberHandler = (e) => {
     setNewNumber(e.target.value);
+  };
+
+  const searchValueHandler = (e) => {
+    setSearchValue(e.target.value);
   };
 
   const formHandler = (e) => {
@@ -26,14 +34,30 @@ const App = () => {
     setNewNumber("");
   };
 
+  const filterHandler = (e) => {
+    e.preventDefault();
+    if (searchValue.length === 0)
+      return window.alert("Please introduce a valid search value");
+    setPersons(
+      persons.filter((person) =>
+        person.name.toLowerCase().includes(searchValue.toLowerCase())
+      )
+    );
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <form onSubmit={filterHandler}>
+        Filter:
+        <input type="text" value={searchValue} onChange={searchValueHandler} />
+        <button type="submit">search</button>
+      </form>
       <form onSubmit={formHandler}>
         <div>
-          name: <input value={newName} onChange={nameHandler} />
+          Name: <input value={newName} onChange={nameHandler} />
           <br />
-          number: <input value={newNumber} onChange={numberHandler} />
+          Number: <input value={newNumber} onChange={numberHandler} />
         </div>
         <div>
           <button type="submit">add</button>
