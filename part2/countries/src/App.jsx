3783ffd7794f7d2fc80753c408f768551cfeb422
asx-dev <asxdev18@gmail.com>
2024-10-23 11:34:41 +0100

    countries exercise

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,14 +1,13 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 function App() {
   const [country, setCountry] = useState("");
   const [countries, setCountries] = useState([]);
-  const [response, setResponse] = useState("");
   const API_URL = "https://studies.cs.helsinki.fi/restcountries/api/name/";
 
   const fetchCountryData = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch(`${API_URL}/${country}`);
+      const response = await fetch(`${API_URL}${country}`);
       const data = await response.json();
       setCountries([data]);
     } catch (error) {
@@ -20,18 +19,6 @@ function App() {
     setCountry(e.target.value);
   };
 
-  useEffect(() => {
-    if (countries.length > 10)
-      setResponse(<p>{"Too many matches, specify another filter."}</p>);
-    if (countries.length > 1 && countries.length < 10)
-      setResponse(
-        countries.map((country) => (
-          <li key={country.name.common}>{country.name.common}</li>
-        ))
-      );
-    if (countries.length === 1) setResponse(countries[0]);
-  }, [countries]);
-
   return (
     <>
       <h1>Countries</h1>
@@ -40,17 +27,30 @@ function App() {
         <input type="text" value={country} onChange={inputHandler} />
         <input type="submit" value="submit" />
       </form>
-      {response.length === 1 && (
-        <>
-          <h1>{response[0].name.common}</h1>
-          <p>Capital:{response[0].capital[0]} </p>
-          <p>Area: {response[0].area}</p>
-          <h1>Languages</h1>
-          {response[0].languages.map((item) => (
-            <lik key={item}>{item}</lik>
-          ))}
-          {response[0].flag}
-        </>
+
+      {/* Many matches */}
+      {countries.length > 10 && (
+        <p>Too many matches, please be more specific.</p>
+      )}
+
+      {/*Between 1 & 10 */}
+      {countries.length > 1 &&
+        countries.length < 10 &&
+        countries.map((country) => {
+          return <li key={country.name.common}>{country.name.common}</li>;
+        })}
+
+      {countries.length === 1 && countries[0].name && (
+        <div>
+          <h2>{countries[0].name.common}</h2>
+          <p>Capital: {countries[0].capital}</p>
+          <p>Area: {countries[0].area}</p>
+          <h3>Languages</h3>
+          {Object.entries(countries[0].languages).map((language) => {
+            return <li key={language[0]}>{language[1]}</li>;
+          })}
+          <span style={{ fontSize: "250px" }}>{countries[0].flag}</span>
+        </div>
       )}
     </>
   );
