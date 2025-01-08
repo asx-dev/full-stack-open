6ffd1fb6597e659315530a36f084152914f4aebe
asx-dev <asxdev18@gmail.com>
2025-01-08 11:47:17 +0000

    Helper Functions and Unit Tests, step 3

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -8,5 +8,14 @@ const totalLikes = (blogs) => {
     : 0;
 };
 
-// For example this part
-module.exports = { dummy, totalLikes };
+const favoriteBlog = (blogs) => {
+  return blogs.length > 0
+    ? blogs.reduce(
+        (favorite, current) =>
+          favorite.likes > current.likes ? favorite : current,
+        blogs[0]
+      )
+    : null;
+};
+
+module.exports = { dummy, totalLikes, favoriteBlog };
